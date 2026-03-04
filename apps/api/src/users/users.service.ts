fa041fc1302bf9fc1caaf5d42b74f6316d02c502
uplooq <uplooq13@gmail.com>
2026-03-04 21:44:30 +0300

    close tri-06

---
+++ apps/api/src/users/users.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, Inject, NotFoundException } from '@nestjs/common'
+import { NodePgDatabase } from 'drizzle-orm/node-postgres'
+import { eq } from 'drizzle-orm'
+import * as schema from '../db/schema'
+import { DRIZZLE } from '../db/db.module'
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @Inject(DRIZZLE) private db: NodePgDatabase<typeof schema>,
+  ) {}
+
+  async findById(id: string) {
+    const user = await this.db.query.users.findFirst({
+      where: eq(schema.users.id, id),
+    })
+    if (!user) throw new NotFoundException('User not found')
+
+    const { passwordHash: _, ...result } = user
+    return result
+  }
+}
