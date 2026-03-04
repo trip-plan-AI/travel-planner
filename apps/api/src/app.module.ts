645768c4a47f67aa44c2de303166fdb0a004f4d3
uplooq <uplooq13@gmail.com>
2026-03-04 21:32:08 +0300

    close tri-04

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { DbModule } from './db/db.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '../../.env' }),
     DbModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
